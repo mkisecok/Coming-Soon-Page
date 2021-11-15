3048dbba0ee8013eff56838668b486727910a489
Mkisecok <mahmutkisecok@gmail.com>
2021-11-15 15:25:23 +0100

    Add style in scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,18 @@ import moment from "moment";
 
 // \/ All of your javascript should go here \/
 
-
+const button=document.querySelector('#button');
 const day = moment().format('Do'); 
 const hours=moment().format('h');
 const minut= moment().format('mm');
 const second=moment().format('ss');
 
+button.addEventListener('click', e=>
+{
+    document.querySelector('#days').innerHTML = `${day} days`;
+    document.querySelector('#hours').innerHTML =`${hours} hours`;
+    document.querySelector('#minuts').innerHTML =`${minut} minuts`;  
+    document.querySelector('#seconds').innerHTML =`${second} seconds`; 
 
+})
 
-document.querySelector('#days').innerHTML = `${day} days`;
-document.querySelector('#hours').innerHTML =`${hours} hours`;
-document.querySelector('#minuts').innerHTML =`${minut} minuts`;  
-document.querySelector('#seconds').innerHTML =`${second} seconds`; 
