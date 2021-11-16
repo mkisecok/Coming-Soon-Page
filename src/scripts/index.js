83228cd6897d7d1144683677c8b255fc1ac0b717
Mkisecok <mahmutkisecok@gmail.com>
2021-11-16 14:01:30 +0100

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,21 +7,22 @@ import moment from "moment";
 // \/ All of your javascript should go here \/
 
 const button=document.querySelector('#button');
-const target = moment().endOf('month')
+const target = moment().endOf('month');
 
 button.addEventListener('click', e=>
 {
 setInterval(function() {
+
     let now = moment().format();
     let diffDuration = moment.duration(target.diff(now));
-    document.querySelector('#days').innerHTML =
-    `${diffDuration.days() } days`;
-    document.querySelector('#hours').innerHTML =
-    `${diffDuration.hours()} hours`;
-    document.querySelector('#minuts').innerHTML =
-    `${diffDuration.minutes()} minutes ` 
-    document.querySelector('#seconds').innerHTML =
-    `${diffDuration.seconds()} seconds` 
+
+    document.querySelector('#days').innerHTML = `${diffDuration.days() } days`;
+
+    document.querySelector('#hours').innerHTML= `${diffDuration.hours()} hours`;
+
+    document.querySelector('#minutes').innerHTML=`${diffDuration.minutes()} minutes`;
+
+    document.querySelector('#seconds').innerHTML=`${diffDuration.seconds()} seconds`;
 }, 1000)})
     
 
